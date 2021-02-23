ac57071a843a9f4938c4c06a9c9a7998ccc5d329
Jonnathan Peña <razaisger@hotmail.com>
2021-02-22 21:25:18 -0500

    prueba en producción

--- dist/src/router/main.router.js
+++ dist/src/router/main.router.js
@@ -2,13 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const response_api_1 = require("../server/response_api");
+const requestApp = require('request');
 const mainRouter = express_1.Router();
 const responseApi = new response_api_1.ResponseApi();
 mainRouter.get('/', (_, w) => {
     responseApi.response(w, 200, { ok: true, response: 'Todo bien' });
 });
 mainRouter.get('/distancematrix', (r, w) => {
-    const url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=-0.156655,-78.4803317&destinations=-0.156442,%20-78.480138&key=AIzaSyBernbrysD5h9fJB2Uws1hermcdf1Bm3sw";
+    requestApp('https://maps.googleapis.com/maps/api/distancematrix/json?origins=-0.156655,-78.4803317&destinations=-0.156442,%20-78.480138&key=AIzaSyBernbrysD5h9fJB2Uws1hermcdf1Bm3sw', function (error, response, body) {
+        console.error('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+        if (!error) {
+            responseApi.response(w, 200, JSON.parse(body));
+        }
+    });
 });
 exports.default = mainRouter;
 //# sourceMappingURL=main.router.js.map
--- src/router/main.router.ts
+++ src/router/main.router.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response, request } from 'express';
 import { ResponseApi } from '../server/response_api';
 
+const requestApp = require('request');
+
 const mainRouter = Router();
 const responseApi = new ResponseApi();
 
@@ -9,8 +11,16 @@ mainRouter.get('/', (_: Request, w: Response) => {
 });
 
 mainRouter.get('/distancematrix', (r: Request, w: Response) => {
-  const url : string = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=-0.156655,-78.4803317&destinations=-0.156442,%20-78.480138&key=AIzaSyBernbrysD5h9fJB2Uws1hermcdf1Bm3sw";
-
+  requestApp('https://maps.googleapis.com/maps/api/distancematrix/json?origins=-0.156655,-78.4803317&destinations=-0.156442,%20-78.480138&key=AIzaSyBernbrysD5h9fJB2Uws1hermcdf1Bm3sw', 
+    function (error: any, response: any, body: any) {
+      console.error('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+      console.log('body:', body);
+      if (!error) {
+        responseApi.response(w, 200, JSON.parse(body));
+      }
+    }
+  );
 });
 
 export default mainRouter;
