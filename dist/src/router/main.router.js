053aff8cc731cfff80d2c4d1bae9205b72d5f636
Jonnathan Peña <razaisger@hotmail.com>
2021-02-22 21:20:59 -0500

    before request install

--- dist/src/router/main.router.js
+++ dist/src/router/main.router.js
@@ -7,5 +7,8 @@ const responseApi = new response_api_1.ResponseApi();
 mainRouter.get('/', (_, w) => {
     responseApi.response(w, 200, { ok: true, response: 'Todo bien' });
 });
+mainRouter.get('/distancematrix', (r, w) => {
+    const url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=-0.156655,-78.4803317&destinations=-0.156442,%20-78.480138&key=AIzaSyBernbrysD5h9fJB2Uws1hermcdf1Bm3sw";
+});
 exports.default = mainRouter;
 //# sourceMappingURL=main.router.js.map
--- src/router/main.router.ts
+++ src/router/main.router.ts
@@ -8,4 +8,9 @@ mainRouter.get('/', (_: Request, w: Response) => {
   responseApi.response(w, 200, {ok: true, response: 'Todo bien'});
 });
 
+mainRouter.get('/distancematrix', (r: Request, w: Response) => {
+  const url : string = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=-0.156655,-78.4803317&destinations=-0.156442,%20-78.480138&key=AIzaSyBernbrysD5h9fJB2Uws1hermcdf1Bm3sw";
+
+});
+
 export default mainRouter;
