a5cbb42bfad7ac43c8a0d29575752c40fdb482b1
Jonnathan Peña <razaisger@hotmail.com>
2021-02-24 22:32:06 -0500

    query request

--- src/router/main.router.ts
+++ src/router/main.router.ts
@@ -11,7 +11,10 @@ mainRouter.get('/', (_: Request, w: Response) => {
 });
 
 mainRouter.get('/distancematrix', (r: Request, w: Response) => {
-  requestApp('https://maps.googleapis.com/maps/api/distancematrix/json?origins=-0.156655,-78.4803317&destinations=-0.156442,%20-78.480138&key=AIzaSyBernbrysD5h9fJB2Uws1hermcdf1Bm3sw', 
+  const origins = r.query.origins;
+  const destinations = r.query.destinations;
+  const key = r.query.key;
+  requestApp(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origins}&destinations=${destinations}&key=${key}`, 
     function (error: any, response: any, body: any) {
       console.error('error:', error);
       console.log('statusCode:', response && response.statusCode);
