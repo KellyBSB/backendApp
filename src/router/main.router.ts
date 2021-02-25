fc5e47ac632fca8cdea0d3d6e460f47f7d7a01e8
KellyBSB <kelly.soto@epn.edu.ec>
2021-02-24 22:55:55 -0500

    direciones segunda funcion

--- src/router/main.router.ts
+++ src/router/main.router.ts
@@ -4,6 +4,9 @@ import { ResponseApi } from '../server/response_api';
 const requestApp = require('request');
 
 const mainRouter = Router();
+const mainRoouterDireciones = Router();
+const mainRouterCalles= Router();
+const mainRouterDistrito = Router();
 const responseApi = new ResponseApi();
 
 mainRouter.get('/', (_: Request, w: Response) => {
@@ -26,4 +29,22 @@ mainRouter.get('/distancematrix', (r: Request, w: Response) => {
   );
 });
 
+mainRoouterDireciones.get('/direcciones',(r:Request, w:Response)=>{
+  const origins = r.query.origins;
+  const destinations = r.query.destinations;
+  const key = r.query.key;
+  requestApp(
+    `https://maps.googleapis.com/maps/api/directions/json?origin=${origins}&destination=${destinations}&mode=walking&key=${key}`,
+    function (error:any, response:any, body:any){
+      console.error('error:',error);
+      console.log('statusCode:', response && response.statusCode );
+      console.log('body:', body);
+      if(!error){
+        responseApi.response(w,200,JSON.parse(body))
+      }
+    }
+
+  );
+
+});
 export default mainRouter;
